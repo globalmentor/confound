12725918896401c6a148701c59f75a0f05ffa29a
Garret Wilson <garret@globalmentor.com>
2018-07-26 20:58:05 -0300

    Added properties-backed configuration implementation.

---
+++ config-properties/readme.md
@@ -0,0 +1,15 @@
+# Properties Configuration
+
+Confound configuration implementation backed by Java properties.
+
+## Download
+
+Properties Configuration is available in the Maven Central Repository as [io.confound:config-properties](https://search.maven.org/#search%7Cga%7C1%7Cg%3A%22oi.confound%22%20AND%20a%3A%22config-properties%22).
+
+## Issues
+
+Issues tracked by [JIRA](https://globalmentor.atlassian.net/projects/CONFOUND).
+
+## Changelog
+
+- 0.5.0: First release.
---
+++ config-properties/src/main/java/io/confound/config/properties/PropertiesConfiguration.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2018 GlobalMentor, Inc. <http://www.globalmentor.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.confound.config.properties;
+
+import static java.util.Objects.*;
+
+import java.util.Optional;
+import java.util.Properties;
+
+import javax.annotation.*;
+
+import io.confound.config.*;
+
+/**
+ * Configuration implementation backed by {@link Properties}.
+ * @author Garret Wilson
+ * @see Properties
+ * @see Properties#getProperty(String)
+ */
+public class PropertiesConfiguration extends AbstractStringConfiguration {
+
+	private final Properties properties;
+
+	/**
+	 * Properties constructor.
+	 * @param properties The properties to back this configuration.
+	 * @throws NullPointerException if the given properties is <code>null</code>.
+	 */
+	public PropertiesConfiguration(@Nonnull final Properties properties) {
+		this(Optional.empty(), properties);
+	}
+
+	/**
+	 * Parent configuration and properties constructor.
+	 * @param parentConfiguration The parent configuration for fallback lookup.
+	 * @param properties The properties to back this configuration.
+	 * @throws NullPointerException if the given parent configuration and/or properties is <code>null</code>.
+	 */
+	public PropertiesConfiguration(@Nonnull final Optional<Configuration> parentConfiguration, @Nonnull final Properties properties) {
+		super(parentConfiguration);
+		this.properties = requireNonNull(properties);
+	}
+
+	/** {@inheritDoc} This implementation delegates to {@link Properties#getProperty(String)}. */
+	@Override
+	protected Optional<String> getOptionalStringImpl(final String key) throws ConfigurationException {
+		return Optional.ofNullable(properties.getProperty(key));
+	}
+
+}
