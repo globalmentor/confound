f048c9e179443bb8b94612b9cbb5ed6c33f1f4e4
Magno Nascimento <magno@globalmentor.com>
2018-11-15 23:44:31 -0200

    CONFOUND-8: Updated JndiConfiguration class to match new version of Configuration.

--- config-jndi/src/main/java/io/confound/config/jndi/JndiConfiguration.java
+++ config-jndi/src/main/java/io/confound/config/jndi/JndiConfiguration.java
@@ -25,7 +25,6 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import io.confound.config.AbstractStringConfiguration;
-import io.confound.config.Configuration;
 import io.confound.config.ConfigurationException;
 
 /**
@@ -40,20 +39,11 @@ public class JndiConfiguration extends AbstractStringConfiguration {
 	private final Context context;
 
 	/**
-	 * Constructor of the JNDI Configuration without any fallback parent configuration or JNDI environment properties.
+	 * Constructor of the JNDI Configuration without any JNDI environment properties.
 	 * @throws NamingException If an error occur while creating an initial JNDI context.
 	 */
 	public JndiConfiguration() throws NamingException {
-		this(null, null);
-	}
-
-	/**
-	 * Constructor of the JNDI Configuration providing a fallback parent configuration but no JNDI environment properties.
-	 * @param parentConfiguration The fallback parent configuration.
-	 * @throws NamingException If an error occur while creating an initial JNDI context.
-	 */
-	public JndiConfiguration(@Nullable final Configuration parentConfiguration) throws NamingException {
-		this(parentConfiguration, null);
+		this(null);
 	}
 
 	/**
@@ -62,24 +52,11 @@ public class JndiConfiguration extends AbstractStringConfiguration {
 	 * @throws NamingException If an error occur while creating an initial JNDI context.
 	 */
 	public JndiConfiguration(@Nullable Hashtable<String, String> environment) throws NamingException {
-		this(null, environment);
-	}
-
-	/**
-	 * Constructor of the JNDI Configuration using a fallback parent configuration and providing JNDI environment properties.
-	 * 
-	 * @param parentConfiguration The fallback parent configuration.
-	 * @param environment The JNDI environment properties to be used.
-	 * @throws NamingException If an error occur while creating an initial JNDI context.
-	 */
-	public JndiConfiguration(@Nullable final Configuration parentConfiguration, @Nullable Hashtable<String, String> environment) throws NamingException {
-		super(parentConfiguration);
-
 		this.context = (Context)(environment == null ? new InitialContext() : new InitialContext(environment));
 	}
 
 	@Override
-	protected Optional<String> findParameterImpl(String key) throws ConfigurationException {
+	protected Optional<String> findConfigurationValueImpl(String key) throws ConfigurationException {
 		try {
 			return Optional.ofNullable(((Context)context.lookup(JNDI_NAMESPACE)).lookup(key)).map(Object::toString);
 		} catch(final NamingException namingException) {
